da838e4e7aeea6557d50808c866de9c9d5996258
Steeely Dan <steeelydan@posteo.de>
2023-07-09 15:36:36 +0200

    Add icon; v0.2.0

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -6,5 +6,5 @@ if [ $# -eq 0 ]
     exit 1
 fi
 
-7z a ./dist/stateful-youtube-"$1".zip ./manifest.json ./public/ ./build/
+7z a ./dist/stateful-youtube-"$1".zip ./manifest.json ./public/ ./build/ ./media/
 echo "Distribution created successfully (version $1)."
