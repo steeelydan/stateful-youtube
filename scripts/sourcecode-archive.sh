dd7c90d0b0115879c7a73c8211ac8c8484a52f28
Steeely Dan <steeelydan@posteo.de>
2023-11-04 13:23:44 +0100

    Add extension icon

--- scripts/sourcecode-archive.sh
+++ scripts/sourcecode-archive.sh
@@ -6,5 +6,5 @@ if [ $# -eq 0 ]
     exit 1
 fi
 
-7z a ./dist/source-code-"$1".zip ./media/ ./public ./src ./manifest.json ./package-lock.json ./package.json ./tsconfig.json ./webpack.config.cjs
+7z a ./dist/source-code-"$1".zip ./art ./media/ ./public ./src ./manifest.json ./package-lock.json ./package.json ./tsconfig.json ./webpack.config.cjs
 echo "Source code archived successfully (version $1)."
