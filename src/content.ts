3429d7bcad7d93c7d77c44c00996beeec4ca0e5c
Steeely Dan <steeelydan@posteo.de>
2023-11-24 20:39:00 +0100

    Revert "Fix jumping to saved time; v0.6.0"
    This reverts commit e4339058c11d9223e8d4d47c168255374f10f36f.

--- src/content.ts
+++ src/content.ts
@@ -4,7 +4,9 @@ import type { HistoryItem, SavedHistoryItems } from './types';
 
 (async (): Promise<void> => {
     let currentUrl = window.location.href;
+    let jumpedToPosition = false;
 
+    let htmlVideoPlayer: HTMLVideoElement | null = null;
     let historyItem: HistoryItem | null = null;
     let titleDiv: HTMLDivElement | null = null;
     let channelLink: HTMLLinkElement | null = null;
@@ -15,35 +17,38 @@ import type { HistoryItem, SavedHistoryItems } from './types';
         const url = new URL(window.location.href);
 
         if (url.pathname === '/watch') {
+            // READING SECTION
+
             // Reset when different video is loaded
             if (url.href !== currentUrl) {
                 currentUrl = url.href;
+                jumpedToPosition = false;
                 historyItem = null;
                 titleDiv = null;
                 channelLink = null;
+                jumpedToPosition = false;
             }
 
             const videoId = url.searchParams.get('v');
             const playlistId = url.searchParams.get('list');
 
             if (!videoId) {
-                console.debug('SFYT: Video id not in url');
-
                 return;
             }
 
-            const htmlVideoPlayer = document.getElementsByTagName('video')[0];
-
-            // I suppose JS rendered elements might still be missing from the page.
             if (!htmlVideoPlayer) {
-                console.debug('SFYT: Video player not found');
+                htmlVideoPlayer = document.getElementsByTagName('video')[0];
+                console.debug('SFYT: Got video player from DOM');
+            }
 
+            // I suppose clientside rendered elements might still be missing from the page.
+            if (!htmlVideoPlayer) {
                 return;
             }
 
-            if (htmlVideoPlayer.paused) {
+            if (jumpedToPosition && htmlVideoPlayer.paused) {
                 console.debug(
-                    `SFYT: Early return because paused: DOM access in interval: ${
+                    `SFYT: Early return: DOM access in interval: ${
                         performance.now() - domAccessBenchStart
                     } ms`
                 );
@@ -55,9 +60,23 @@ import type { HistoryItem, SavedHistoryItems } from './types';
                 'video_' + videoId
             )) as SavedHistoryItems;
 
+            console.debug('SFYT: Got historyItem from storage');
+
             if (savedHistoryItem) {
                 historyItem = savedHistoryItem['video_' + videoId];
             }
+
+            // We do not abort if historyItem missing - in this case we've simply not seen the video before.
+
+            //  Jump to current time only if video seen before
+            if (historyItem && !jumpedToPosition) {
+                htmlVideoPlayer.currentTime = Math.floor(historyItem.time);
+                jumpedToPosition = true;
+
+                return;
+            }
+
+            // WRITING SECTION
 
             const time = htmlVideoPlayer.currentTime;
             const duration = htmlVideoPlayer.duration;
@@ -66,11 +85,11 @@ import type { HistoryItem, SavedHistoryItems } from './types';
                 titleDiv = document.querySelector(
                     '.watch-active-metadata div#title yt-formatted-string'
                 ) as HTMLDivElement;
+
+                console.debug('SFYT: Got title div from DOM');
             }
 
             if (!titleDiv) {
-                console.debug('SFYT: Title div not found');
-
                 return;
             }
 
@@ -78,11 +97,11 @@ import type { HistoryItem, SavedHistoryItems } from './types';
                 channelLink = document.querySelector(
                     '.watch-active-metadata yt-formatted-string.ytd-channel-name a'
                 ) as HTMLLinkElement;
+
+                console.debug('SFYT: Got channel link from DOM');
             }
 
             if (!channelLink) {
-                console.debug('SFYT: Channel link not found');
-
                 return;
             }
 
@@ -98,6 +117,8 @@ import type { HistoryItem, SavedHistoryItems } from './types';
             };
 
             await upsertHistoryItem(videoId, historyItem);
+
+            jumpedToPosition = true;
         }
 
         console.debug(
